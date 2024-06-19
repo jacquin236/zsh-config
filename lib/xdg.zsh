01fdf03a5c60c517d7267e0093fc94534b7ab0a0
Jacquin Moon <bumbabee@DESKTOP-913PQCH>
2024-06-19 19:02:30 +0700

    add nvm dir

--- lib/xdg.zsh
+++ lib/xdg.zsh
@@ -46,6 +46,11 @@ alias gpg="${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\""
 # jupyter
 export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
 
+# nvm
+export NVM_DIR="${NVM_DIR:-$XDG_CONFIG_HOME/nvm}"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # node
 export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-$XDG_CONFIG_HOME/npm/npmrc}"
 export NODE_REPL_HISTORY="${NODE_REPL_HISTORY:-$XDG_DATA_HOME/nodejs/repl_history}"
